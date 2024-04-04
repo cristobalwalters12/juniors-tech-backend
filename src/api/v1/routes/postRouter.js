d19a687c6abee942e7a1b4b67f70dee44e303a20
Ayxa Chaverra <achaverrar@gmail.com>
2024-04-04 07:43:16 -0500

    add the postExists middleware to the edit post feature validations

--- src/api/v1/routes/postRouter.js
+++ src/api/v1/routes/postRouter.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { errorCatcher } from '../../helpers/index.js'
-import { methodNotAllowedHandler, uidValidator } from '../middleware/index.js'
+import { methodNotAllowedHandler, postExists, uidValidator } from '../middleware/index.js'
 import { createPost, getPostById, getPosts, editPostById } from '../controllers/postController.js'
 import { postDto } from '../dtos/postDto.js'
 import { mockUser } from '../middleware/mockUser.js'
@@ -16,7 +16,12 @@ router
 router
   .route('/:id')
   .get([mockUser, uidValidator], errorCatcher(getPostById))
-  .put([mockUser, uidValidator, postDto], errorCatcher(editPostById))
+  .put([
+    mockUser,
+    uidValidator,
+    postExists,
+    postDto
+  ], errorCatcher(editPostById))
   .all(methodNotAllowedHandler)
 
 export default router
