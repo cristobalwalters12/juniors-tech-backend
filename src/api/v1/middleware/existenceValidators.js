d20cdf419c15264430191568afba7be82a01ca6a
Ayxa Chaverra <achaverrar@gmail.com>
2024-04-04 23:41:58 -0500

    refactor the post existence validation logic

--- src/api/v1/middleware/existenceValidators.js
+++ src/api/v1/middleware/existenceValidators.js
@@ -1,6 +1,13 @@
-import { existenceValidatorBuilder } from '../../helpers/existenceValidatorBuilder.js'
-import { existsById } from '../models/postModel.js'
+import { AppError } from '../../helpers/AppError.js'
+import { existsById as existsPostById } from '../models/postModel.js'
 
-const postExists = existenceValidatorBuilder(existsById, 'La publicación no existe')
+const postExists = async (req, res, next) => {
+  const resource = await existsPostById(req.params.id)
+  if (resource === undefined) {
+    return next(AppError.notFound('La publicación no existe'))
+  }
+  req.resource = resource
+  next()
+}
 
 export { postExists }
