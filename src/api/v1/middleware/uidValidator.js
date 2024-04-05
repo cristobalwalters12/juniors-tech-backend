5a554352b015cdf845cbd540be93f50b2eafdb86
Ayxa Chaverra <achaverrar@gmail.com>
2024-04-04 23:49:49 -0500

    refactor the uidSchema to make it reusable

--- src/api/v1/middleware/uidValidator.js
+++ src/api/v1/middleware/uidValidator.js
@@ -2,13 +2,13 @@ import Joi from 'joi'
 import { pathVariablesValidatorBuilder } from '../../helpers/index.js'
 
 const uidSchema = Joi.string().trim().length(10).messages({
-  'string.length': 'El id ingresado es inválido',
-  'string.empty': 'El id no puede estar vacío',
-  'any.required': 'El id es obligatorio'
-})
+  'string.length': '{{#label}} ingresado es inválido',
+  'string.empty': '{{#label}} no puede estar vacío',
+  'any.required': '{{#label}} es obligatorio'
+}).required()
 
-const validate = async ({ params: { id } }) => await uidSchema.validateAsync(id)
+const validate = async ({ params: { id } }) => await uidSchema.label('El id').validateAsync(id)
 
 const uidValidator = pathVariablesValidatorBuilder(validate)
 
-export { uidValidator }
+export { uidValidator, uidSchema }
