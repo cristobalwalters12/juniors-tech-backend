102489077963709d2d0e6f80730d805c552c312f
Ayxa Chaverra <achaverrar@gmail.com>
2024-04-05 00:50:26 -0500

    fix typo in the vote table and include the delete_at missing column from the aspect table

--- src/database/DDL.sql
+++ src/database/DDL.sql
@@ -130,6 +130,7 @@ CREATE TABLE "aspect" (
   "comment_count" INTEGER DEFAULT 0 NOT NULL,
   "created_at" TIMESTAMP DEFAULT (NOW()) NOT NULL,
   "updated_at" TIMESTAMP,
+  "deleted_at" TIMESTAMP,
   "has_open_report" BOOLEAN DEFAULT FALSE NOT NULL,
 
   CONSTRAINT existent_aspect_type FOREIGN KEY (aspect_type_id) REFERENCES aspect_type (id),
