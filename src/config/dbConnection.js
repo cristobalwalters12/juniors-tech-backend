f118f522f9faaca9b7287a14d20ba9b0966d1646
cristobalwalters12 <cristobalwalters@gmail.com>
2024-04-08 21:30:40 -0400

    Implementar la conexión a la base de datos utilizando la variable de entorno DATABASE_URL en lugar de las variables separadas de host, usuario, contraseña, puerto y base de datos

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -1,13 +1,9 @@
 import 'dotenv/config'
 import pg from 'pg'
 
-import { host, user, password, port, database } from '../config/envs.js'
+import { DATABASE_URL } from '../config/envs.js'
 const pool = new pg.Pool({
-  user,
-  password,
-  host,
-  port,
-  database,
+  connectionString: DATABASE_URL,
   allowExitOnIdle: true
 })
 
