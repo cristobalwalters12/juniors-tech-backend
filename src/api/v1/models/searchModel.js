ef629c5097c5ab48fd9b3888b7321ec2277c25fe
cristobalwalters12 <cristobalwalters@gmail.com>
2024-04-09 20:37:20 -0400

    Corrige manejo de errores en el controlador de búsqueda

--- src/api/v1/models/searchModel.js
+++ src/api/v1/models/searchModel.js
@@ -21,8 +21,13 @@ const searchModel = async (query) => {
 
   const data = []
 
+  let searchFieldsCount = 0
   for (const field of fields) {
     if (query[field]) {
+      searchFieldsCount++
+      if (searchFieldsCount > 1) {
+        throw new Error('Solo se puede buscar un tipo de dato a la vez')
+      }
       const snakeCaseField = toSnakeCase(field)
       const result = await pool.query(`SELECT id, name FROM ${snakeCaseField} WHERE name ILIKE $1`, [`%${query[field]}%`])
       data.push(...result.rows)
