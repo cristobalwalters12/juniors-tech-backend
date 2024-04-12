9d19060e38db7e454619790581b8cd22d2a50e9e
cristobalwalters12 <cristobalwalters@gmail.com>
2024-04-12 16:37:35 -0400

    Agrega soporte para SSL en la conexión a la base de datos en dbConnection.js

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -1,12 +1,26 @@
 import 'dotenv/config'
 import pg from 'pg'
 
-import { DATABASE_URL } from '../config/envs.js'
-const pool = new pg.Pool({
-  connectionString: DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-})
+import { DB_HOST, DB_NAME, DB_PORT, DB_USER, DB_PASSWORD, DATABASE_URL } from '../config/envs.js'
+
+let pool
+
+if (process.env.NODE_ENV === 'production') {
+  pool = new pg.Pool({
+    connectionString: DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  })
+} else {
+  pool = new pg.Pool({
+    user: DB_USER,
+    host: DB_HOST,
+    database: DB_NAME,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+    allowExitOnIdle: true
+  })
+}
 
 export { pool }
