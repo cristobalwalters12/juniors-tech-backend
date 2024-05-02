9b4c6f69c9a080028fec1cac346fec7398890fab
cristobalwalters12 <cristobalwalters@gmail.com>
2024-05-02 09:15:08 -0400

    Corregir errores relacionados con las funciones de búsqueda

--- src/config/envs.js
+++ src/config/envs.js
@@ -3,7 +3,10 @@ import 'dotenv/config'
 const NODE_ENV = process.env.NODE_ENV
 
 const PORT = process.env.PORT || 3000
-const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN
+const ALLOWED_ORIGIN = [
+  process.env.ALLOWED_ORIGIN,
+  process.env.ALLOWED_ORIGIN2
+]
 const CORS_ALLOW_ALL = process.env.CORS_ALLOW_ALL === 'true'
 const DATABASE_URL = process.env.DATABASE_URL
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET
@@ -25,5 +28,4 @@ export {
   DB_HOST,
   DB_PORT,
   CORS_ALLOW_ALL
-
 }
